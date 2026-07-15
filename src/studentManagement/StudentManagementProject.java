6a852aa29fbc18800625313a0fd8823fdf54c3f0
Akanksha Devi <akankshapundir13@gmail.com>
2026-07-15 18:27:47 +0530

    Refactor application with menu-driven interface

--- src/studentManagement/StudentManagementProject.java
+++ src/studentManagement/StudentManagementProject.java
@@ -4,12 +4,41 @@ import java.util.Scanner;
 public class StudentManagementProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Student> students = new ArrayList<>();
-		
 		boolean isRunning = true;
+
 		while(isRunning) {
+			System.out.println("======Student Management System======");
+			System.out.println("Which operation do you want to do!");
+			System.out.println("1. Add details of new student.");
+			System.out.println("2. Display details of students.");
+			System.out.println("3. Exit");
+			
+			int choice = sc.nextInt();
+			sc.nextLine();
+			
+			switch(choice) {
+			case 1 : addStudent(students, sc);
+			break;
+			
+			case 2 : displayStudents(students);
+			break;
+			
+			case 3 : isRunning = false;
+			     System.out.println("\nThank you for using the Student Management System.");
+			     System.out.println("Have a great day!");
+				break;
+				
+			default:
+			    System.out.println("Invalid choice! Please enter a valid option.");
+			    System.out.println();
+			}
+		}
+		sc.close();
+	}
+	
+	public static void addStudent(ArrayList<Student> students, Scanner sc) {
 			System.out.print("Enter Name: ");
 			String name = sc.nextLine();
 			
@@ -18,7 +47,7 @@ public class StudentManagementProject {
 
 			System.out.print("Enter Age: ");
 			int age = sc.nextInt();
-			sc.nextLine(); // consume the leftover newline
+			sc.nextLine(); 
 
 			System.out.print("Enter Course: ");
 			String course = sc.nextLine();
@@ -30,21 +59,20 @@ public class StudentManagementProject {
 			
 			students.add(s);
 			System.out.println("Student added successfully!");
-			System.out.println("1. Add Another Student");
-		    System.out.println("2. Exit");
-			
-			int choice = sc.nextInt();
-			sc.nextLine();
-			if(choice == 2) {
-				isRunning = false;
-			}
+			System.out.println();
+	}
+	
+	public static void displayStudents(ArrayList<Student> students) {
+		if(students.isEmpty()) {
+			System.out.println("No students available.");
+			System.out.println();
+			return;
 		}
-
-		for(Student s1: students) {
-			s1.displayDetails();
+		
+		for(Student student: students) {
+			student.displayDetails();
 			System.out.println();
 		}
-
 	}
 
 }
