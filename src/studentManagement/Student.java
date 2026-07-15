27ca22df97e64273cb983eea19797ff469554db4
Akanksha Devi <akankshapundir13@gmail.com>
2026-07-15 18:45:25 +0530

    Implement encapsulation using getter and setter

--- src/studentManagement/Student.java
+++ src/studentManagement/Student.java
@@ -15,6 +15,47 @@ public class Student {
 		this.fName = fName;
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	
 	void displayDetails() {
 	    System.out.println("----------------------------");
 	    System.out.println("Name          : " + name);
